7faf4ae3d6e802c477525757fd2b6b23a640a23d
hosanglee-kr <50106977+hosanglee-kr@users.noreply.github.com>
2024-06-14 12:06:39 +0000

    audio tool example add

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,22 @@
 #define STR_VALUE(arg) STRINGIZER(arg)
 #define VERSION STR_VALUE(BUILD_VERSION)
 
+
+#include "S10_streams-i2s-webserver_wav_001.h"
+
 void setup(){
   delay(5000);
 
   Serial.begin(115200);
   Serial.print("Version: ");
   Serial.println(VERSION);
+
+  S10_setup();
+
 }
 
 void loop(){
 
+  S10_loop();
+  
 }
