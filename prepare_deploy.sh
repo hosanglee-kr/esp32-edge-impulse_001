964e7ff92c848d3e7fdb99e304787d7a05113b48
hosang lee <2540@paran.com>
2024-06-17 00:47:46 +0900

    aa

--- prepare_deploy.sh
+++ prepare_deploy.sh
@@ -19,7 +19,7 @@ rename 's/.pio-build-//' .*.bin
 # remove binaries for *-all*, *-test* env and only zip containing *-test*
 rm -f *-all*.bin *-test*.bin *-test*.zip
 echo "zipping code and licence"
-zip -r OpenMQTTGateway_sources.zip src
+zip -r Sources.zip src
 mv *.zip toDeploy
 mv *.bin toDeploy
 
