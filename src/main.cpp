f06161722e5ecd2f5377d9d12472f9c267d7cdb2
hosang lee <2540@paran.com>
2024-06-18 19:41:58 +0900

    add example "B10_esp32_microphone_001."

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,23 @@
 #include <Arduino.h>
 
-// The build version comes from an environment variable. Use the VERSION
-// define wherever the version is needed.
-#define STRINGIZER(arg) #arg
-#define STR_VALUE(arg) STRINGIZER(arg)
-#define VERSION STR_VALUE(BUILD_VERSION)
+#define B10
 
-#define S10
-#ifdef S10
-    #include "S10_streams-i2s-webserver_wav_001.h"
+#ifdef B10
+	#include "B10_esp32_microphone_001.h"
 #endif
 
-void setup(){
-  //delay(5000);
+void setup() {
+	Serial.begin(115200);
 
-  Serial.begin(115200);
-  Serial.print("Version: ");
-  Serial.println(VERSION);
-  
-  #ifdef S10
-    S10_setup();
-  #endif
-  
-  Serial.println("11111");
+	#ifdef B10
+		B10_init();
+	#endif
 
+	Serial.println("11111");
 }
 
-void loop(){
-  #ifdef S10
-      S10_loop();
-  #endif
-  
+void loop() {
+	#ifdef B10
+		B10_init();
+	#endif
 }
