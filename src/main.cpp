fced57df05365611ac25f193a81ba578314d05af
hosang lee <2540@paran.com>
2024-06-21 19:11:37 +0900

    add static_buffer example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 
-#define B10
+#define B05
+
+#ifdef B05
+	#include "B05_static_buffer_001.h"
+#endif
+
+//#define B10
 
 #ifdef B10
 	#include "B10_esp32_microphone_001.h"
@@ -9,14 +15,24 @@
 void setup() {
 	Serial.begin(115200);
 
+	#ifdef B05
+		B05_init();
+	#endif
+
 	#ifdef B10
 		B10_init();
 	#endif
+
+
 
 	Serial.println("11111");
 }
 
 void loop() {
+	#ifdef B05
+		B05_init();
+	#endif
+
 	#ifdef B10
 		B10_init();
 	#endif
